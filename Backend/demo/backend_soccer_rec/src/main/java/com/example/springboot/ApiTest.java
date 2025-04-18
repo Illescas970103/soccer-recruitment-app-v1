9fecdaaf78579d62a0e614a0c06d71fc9cb3f880
Luis Alfredo Illescas Sanchez <alfredoillescas@MacBook-Air-de-Alfredo.local>
2025-04-18 12:03:43 -0600

    segundo cambio de backend

--- Backend/demo/backend_soccer_rec/src/main/java/com/example/springboot/ApiTest.java
+++ Backend/demo/backend_soccer_rec/src/main/java/com/example/springboot/ApiTest.java
@@ -2,12 +2,10 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-public class HelloController {
-    @GetMapping
-    public List<String> hello(){
-        return List.of("Hello","From the Backend");
+public class ApiTest {
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "Hola desde el backend";
     }
 }
